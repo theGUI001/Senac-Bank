27387fe60f9839a9d6de2287ebff477cb915486b
theGUI001 <lghmelo@gmail.com>
2022-05-19 15:58:37 -0300

    Classe cliente adicionada

--- Classes.py
+++ Classes.py
@@ -7,3 +7,12 @@ class Conta:
 
     def __str__(self):
         return f'Conta Número: {self.numero}\nTitular: {self.titular}\nSaldo Atual: {self.saldo}\nLimite da Conta:{self.limite}'
+
+class Cliente:
+    def __init__(self, nome, sobrenome, cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+    
+    def __str__(self):
+        return f'Nome Completo: {self.nome} {self.sobrenome}\n CPF: {self.cpf}'
